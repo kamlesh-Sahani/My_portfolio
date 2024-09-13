a79649ebccaab2d60d2b4dc8e839a72ad9d2255e
kamlesh sahani <alex20150001969@gmail.com>
2024-09-13 23:00:09 +0530

    env update

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import About from './Container/About/About';
 import Work from './Container/Work/Work';
 import Skill from './Container/Skill/Skill';
 const App = () => {
-  console.log(process.env.REACT_APP_SANITY_PROJECT_ID,"project id")
   return (
     <div className="app">
          <Navbar />
